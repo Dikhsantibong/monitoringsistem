d34e880aadb7bb549ebaedf27291eb20582a494c
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2024-12-31 18:12:10 +0800

    tesstt

--- resources/views/admin/laporan/sr_wo.blade.php
+++ resources/views/admin/laporan/sr_wo.blade.php
@@ -33,7 +33,9 @@
                             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
-                    <h1 class="text-xl font-semibold text-gray-800">Laporan SR/WO test</h1>
+                    <h1 class="text-xl font-semibold text-gray-800">Laporan SR/WO
+                        
+                    </h1>
                     </div>
                     
                     @include('components.timer')
