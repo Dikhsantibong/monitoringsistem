83e85a72d44e7f3620f664ef22b9e4ed6e543598
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-07-21 09:10:12 +0800

    perbaikan

--- resources/views/admin/machine-status/_table.blade.php
+++ resources/views/admin/machine-status/_table.blade.php
@@ -190,9 +190,7 @@
                                 <th class="px-3 py-2.5 bg-[#0A749B] text-white text-sm font-medium tracking-wider text-center border-r border-[#0A749B]">Progress</th>
                                 <th class="px-3 py-2.5 bg-[#0A749B] text-white text-sm font-medium tracking-wider text-center border-r border-[#0A749B]">Tanggal Mulai</th>
                                 <th class="px-3 py-2.5 bg-[#0A749B] text-white text-sm font-medium tracking-wider text-center">Target Selesai</th>
-                                @if(auth()->user()->role === 'super_admin')
                                 <th class="px-3 py-2.5 bg-[#0A749B] text-white text-sm font-medium tracking-wider text-center">Aksi</th>
-                                @endif
                             </tr>
                         </thead>
                         <tbody class="text-sm">
