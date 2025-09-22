f6c84a5f4702522e917b89f8fee0ac005a3338aa
Dikhsantibong <tibongdikhsan@gmail.com>
2025-09-23 00:57:21 +0500

    perbaikan

--- resources/views/pemeliharaan/labor-saya.blade.php
+++ resources/views/pemeliharaan/labor-saya.blade.php
@@ -190,6 +190,7 @@
                         <thead class="bg-gray-100">
                             <tr>
                                 <th class="px-4 py-2 text-center">No</th>
+                                <th class="px-4 py-2 text-center">Aksi</th>
                                 <th class="px-4 py-2 text-center">No WO</th>
                                 <th class="px-4 py-2 text-center">Deskripsi</th>
                                 <th class="px-4 py-2 text-center">Kendala</th>
@@ -202,13 +203,19 @@
                                 <th class="px-4 py-2 text-center">Jadwal Selesai</th>
                                 <th class="px-4 py-2 text-center">Labor</th>
                                 <th class="px-4 py-2 text-center">Nama Labor</th>
-                                <th class="px-4 py-2 text-center">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
                             @forelse($laborBacklogs as $backlog)
                             <tr class="hover:bg-gray-50">
                                 <td class="px-4 py-2 text-center border border-gray-200">{{ $loop->iteration }}</td>
+                                <td class="px-4 py-2 border border-gray-200 text-center">
+                                    @if($backlog->status == 'Closed')
+                                        <span class="inline-block px-3 py-1 bg-gray-400 text-white rounded text-xs cursor-not-allowed">Closed</span>
+                                    @else
+                                        <a href="{{ route('pemeliharaan.labor-saya.edit-backlog', $backlog->id) }}" class="inline-block px-3 py-1 bg-yellow-400 text-white rounded hover:bg-yellow-500 text-xs"><i class="fas fa-edit"></i> Edit</a>
+                                    @endif
+                                </td>
                                 <td class="px-4 py-2 border border-gray-200">{{ $backlog->no_wo }}</td>
                                 <td class="px-4 py-2 border border-gray-200 max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap">{{ $backlog->deskripsi }}</td>
                                 <td class="px-4 py-2 border border-gray-200">{{ $backlog->kendala }}</td>
@@ -243,13 +250,7 @@
                                         -
                                     @endif
                                 </td>
-                                <td class="px-4 py-2 border border-gray-200 text-center">
-                                    @if($backlog->status == 'Closed')
-                                        <span class="inline-block px-3 py-1 bg-gray-400 text-white rounded text-xs cursor-not-allowed">Closed</span>
-                                    @else
-                                        <a href="{{ route('pemeliharaan.labor-saya.edit-backlog', $backlog->id) }}" class="inline-block px-3 py-1 bg-yellow-400 text-white rounded hover:bg-yellow-500 text-xs"><i class="fas fa-edit"></i> Edit</a>
-                                    @endif
-                                </td>
+                               
                             </tr>
                             @empty
                             <tr>
