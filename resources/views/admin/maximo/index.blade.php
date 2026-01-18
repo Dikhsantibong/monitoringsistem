d2b9d09624f2d3f76bb22ce98bcddf1c6d92c5b8
Dikhsantibong <tibongdikhsan@gmail.com>
2026-01-19 03:05:03 +0800

    tes jobcard

--- resources/views/admin/maximo/index.blade.php
+++ resources/views/admin/maximo/index.blade.php
@@ -203,7 +203,7 @@
                                         </td>
                                         <td class="px-4 py-2 text-center border border-gray-200 whitespace-nowrap">
                                             <div class="flex items-center justify-center gap-2">
-                                                <a href="{{ route('maximo.workorder.show', ['wonum' => $wo['wonum']]) }}"
+                                                <a href="{{ route('admin.maximo.workorder.show', ['wonum' => $wo['wonum']]) }}"
                                                    class="inline-flex items-center px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 text-xs">
                                                     Detail
                                                 </a>
