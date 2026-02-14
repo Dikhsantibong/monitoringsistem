a47d458254b6c2a99987b1048c7aead50c0664c1
Dikhsantibong <tibongdikhsan@gmail.com>
2026-02-14 13:21:47 +0800

    perbaikan jobcard

--- resources/views/components/pemeliharaan-sidebar.blade.php
+++ resources/views/components/pemeliharaan-sidebar.blade.php
@@ -17,6 +17,11 @@
                 <i class="fas fa-tools w-6 h-6"></i>
                 <span class="ml-3 text-base">Dashboard Pemeliharaan</span>
             </a>
+            <a href="{{ route('pemeliharaan.calendar') }}"
+                class="flex items-center px-3 py-2.5 rounded-lg {{ request()->routeIs('pemeliharaan.calendar') ? 'bg-white/10 text-white font-medium' : 'text-gray-100 hover:bg-white/10' }}">
+                <i class="fas fa-calendar-alt w-6 h-6"></i>
+                <span class="ml-3 text-base">Kalender Pemeliharaan</span>
+            </a>
 
             <a href="{{ route('pemeliharaan.labor-saya') }}"
                 class="flex items-center px-3 py-2.5 rounded-lg {{ request()->routeIs('pemeliharaan.labor-saya') ? 'bg-white/10 text-white font-medium' : 'text-gray-100 hover:bg-white/10' }}">
@@ -43,11 +48,7 @@
                 <i class="fas fa-file-upload w-6 h-6"></i>
                 <span class="ml-3 text-base">Pengajuan Material</span>
             </a>
-            <a href="{{ route('pemeliharaan.calendar') }}"
-                class="flex items-center px-3 py-2.5 rounded-lg {{ request()->routeIs('pemeliharaan.calendar') ? 'bg-white/10 text-white font-medium' : 'text-gray-100 hover:bg-white/10' }}">
-                <i class="fas fa-calendar-alt w-6 h-6"></i>
-                <span class="ml-3 text-base">Kalender Pemeliharaan</span>
-            </a>
+            
            
             <!-- <a href="{{ route('pemeliharaan.master-labor') }}"
                 class="flex items-center px-3 py-2.5 rounded-lg {{ request()->routeIs('pemeliharaan.master-labor') ? 'bg-white/10 text-white font-medium' : 'text-gray-100 hover:bg-white/10' }}">
